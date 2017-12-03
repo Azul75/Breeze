c41561112809e05f7db038a1f433ba698510f43b
Haro <jonedharo@hotmail.com>
2017-12-03 01:54:08 -0600

    Puse de nuevo el email

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -3,14 +3,12 @@
 	$email=$_REQUEST["e"];
 	$phone=$_REQUEST["p"];
 	$comment=$_REQUEST["m"];
-	$lan=$_REQUEST["lan"];
 
 	if($email==""||$comment==""||strlen($name)>40||strlen($email)>40||strlen($phone)>10||strlen($comment)>100){
-		echo "string";
 		exit();
 	}
 
-	require __DIR__.'/PHPMailer/PHPMailerAutoload.php';
+	require __DIR__.'\PHPMailer\PHPMailerAutoload.php';
 
 	ob_start();
 	$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
@@ -20,41 +18,23 @@
 	    $mail->isSMTP();                                      // Set mailer to use SMTP
 	    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 	    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-	    $mail->Username = 'mexitaco44@gmail.com';                 // SMTP username
-	    $mail->Password = 'turbopass9';                           // SMTP password
+	    $mail->Username = 'mexitaco44@gmail.com';                 // Email que envia
+	    $mail->Password = 'turbopass9';                           // Su contraseña
 	    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	    $mail->Port = 587;                                    // TCP port to connect to
 
 	    //Recipients
-	    $mail->setFrom('mexitaco44@gmail.com', 'Breeze');
-	    $mail->addAddress('mexitaco44@gmail.com', 'Breeze');
+	    $mail->setFrom('mexitaco44@gmail.com', 'Breeze');  
+	    $mail->addAddress('mexitaco44@gmail.com', 'Breeze'); //Email destino
 
 	    //Content
 	    $mail->isHTML(true);                                  // Set email format to HTML
 	    $mail->Subject ='Breeze';
-	    $mail->Body    ='Nombre: '.$name.'</br>Email: '.$email.'</br>Telefono: '.$phone.'</br>Comentario: '.$comment;
+	    $mail->Body    ='Nombre: '.$name.'<br>Email: '.$email.'<br>Telefono: '.$phone.'<br>Comentario: '.$comment;
 	    $mail->AltBody ='This is the body in plain text for non-HTML mail clients';
 
 	    $mail->send();
 	} catch (Exception $e) {
 	}
 	ob_end_clean();
-
-	$db=new mysqli('localhost',"root","","Mail") or die("");
-	$entry="INSERT INTO Message(name,email,phone,comment) VALUES ('$name','$email','$phone','$comment')";
-	if ($db->query($entry)===TRUE){
-		if($lan=="es"){
-	    	echo "Gracias por su preferencia";
-		}else{
-			echo "Error: Mensaje no enviado";
-		}
-	}else{
-		if($lan=="es"){
-	    	echo "Thanks for your preference";
-		}else{
-			echo "Error: Message not sent";
-		}		
-	}
-
-	$db->close();
 ?>
