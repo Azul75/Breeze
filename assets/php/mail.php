79759f2f8eb90da64b3554d02cf9ffdca901bb4f
Azul75 <jlgpblue@gmail.com>
2017-12-05 17:32:14 -0600

    ajustes

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,4 +1,6 @@
 <?php
+    require_once(__DIR__.'/PHPMailer/PHPMailerAutoload.php');
+    
 	$name=$_REQUEST["n"];
 	$email=$_REQUEST["e"];
 	$phone=$_REQUEST["p"];
@@ -8,31 +10,22 @@
 		exit();
 	}
 
-	require __DIR__.'\PHPMailer\PHPMailerAutoload.php';
-
-	
 	$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 	try {
 	    //Server settings
-	    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+	    #$mail->SMTPDebug = 2;                                 // Enable verbose debug output
 	    $mail->isSMTP();                                      // Set mailer to use SMTP
-	    $mail->SMTPOptions = array(
-		    'ssl' => array(
-		        'verify_peer' => false,
-		        'verify_peer_name' => false,
-		        'allow_self_signed' => true
-		    )
-		);
-	    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+	    $mail->Host = 'send.one.com';  // Specify main and backup SMTP servers
 	    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-	    $mail->Username = 'mexitaco44@gmail.com';                 // Email que envia
-	    $mail->Password = 'turbopass9';                           // Su contraseña
+	    $mail->Username = 'no-reply@breezepv.mx';                 // Email que envia
+	    $mail->Password = 'gioDirt240';                           // Su contraseña
 	    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	    $mail->Port = 587;                                    // TCP port to connect to
 
 	    //Recipients
-	    $mail->setFrom('mexitaco44@gmail.com', 'Breeze');  
-	    $mail->addAddress('mexitaco44@gmail.com', 'Breeze'); //Email destino
+	    $mail->setFrom('no-reply@breezepv.mx', 'Breeze No-Reply');  
+	    $mail->addAddress('info@breezepv.mx', 'Breeze Info'); //Email destino
+	    $mail->addAddress('mexitaco44@gmail.com', 'Washines Co. Ltd');
 
 	    //Content
 	    $mail->isHTML(true);                                  // Set email format to HTML
@@ -42,7 +35,6 @@
 
 	    $mail->send();
 	} catch (Exception $e) {
-	    echo 'Message could not be sent.';
-    	echo 'Mailer Error: ' . $mail->ErrorInfo;
+	    echo "Error: ";print_r($e);
 	}
 ?>
